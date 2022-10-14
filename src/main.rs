774370aadf97036615ac5d2d65a7d08854ca7bce
Dustin Brown <me@dubrowgn.com>
2022-10-14 14:45:52 -0600

    Show empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+	App::new()
+		.insert_resource(ClearColor(Color::rgb(1.0, 1.0, 1.0)))
+		.insert_resource(WindowDescriptor {
+			title: "test".to_string(),
+			..default()
+		})
+		.add_plugins(DefaultPlugins)
+		.run();
 }
